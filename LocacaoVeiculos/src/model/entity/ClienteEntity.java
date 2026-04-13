c65793fa3aeb0dc0a23fe0fc777bb8325b2e3d54
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2026-04-13 14:58:05 -0300

    Update ClienteEntity.java

--- LocacaoVeiculos/src/model/entity/ClienteEntity.java
+++ LocacaoVeiculos/src/model/entity/ClienteEntity.java
@@ -1,41 +1,22 @@
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-@Entity
-@Table(name = "tb_cliente")
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
-public class ClienteEntity {
+public class Cliente {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @EqualsAndHashCode.Include
     private Long id;
 
-    @Column(nullable = false, unique = true, length = 11)
     private String cpf;
-
-    @Column(nullable = false, length = 100)
     private String nome;
-
-    @Column(length = 255)
     private String endereco;
-
-    @Column(length = 20)
     private String telefone;
-
-    @Column(nullable = false, unique = true, length = 100)
     private String email;
 }
