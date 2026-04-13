3bc85f2b36391b95c78240def0fdf08743a4c0f3
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2026-04-13 15:05:03 -0300

    Refactor AutomovelEntity class to remove Lombok annotations

--- LocacaoVeiculos/src/model/entity/AutomovelEntity.java
+++ LocacaoVeiculos/src/model/entity/AutomovelEntity.java
@@ -1,21 +1,9 @@
-import lombok.AllArgsConstructor;
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-
 import java.math.BigDecimal;
+import java.util.Objects;
 
-@Getter
-@Setter
-@NoArgsConstructor
-@AllArgsConstructor
-@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class Automovel {
 
-    @EqualsAndHashCode.Include
     private Long id;
-    
     private String placa;
     private String renavam;
     private String chassi;
@@ -25,4 +13,113 @@ public class Automovel {
     private Long quilometragem;
     private BigDecimal valorLocacao;
     private Modelo modelo;
+
+    public Automovel() {
+    }
+
+    public Automovel(Long id, String placa, String renavam, String chassi, String cor, Integer numeroPortas, Integer tipoCombustivel, Long quilometragem, BigDecimal valorLocacao, Modelo modelo) {
+        this.id = id;
+        this.placa = placa;
+        this.renavam = renavam;
+        this.chassi = chassi;
+        this.cor = cor;
+        this.numeroPortas = numeroPortas;
+        this.tipoCombustivel = tipoCombustivel;
+        this.quilometragem = quilometragem;
+        this.valorLocacao = valorLocacao;
+        this.modelo = modelo;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getRenavam() {
+        return renavam;
+    }
+
+    public void setRenavam(String renavam) {
+        this.renavam = renavam;
+    }
+
+    public String getChassi() {
+        return chassi;
+    }
+
+    public void setChassi(String chassi) {
+        this.chassi = chassi;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public Integer getNumeroPortas() {
+        return numeroPortas;
+    }
+
+    public void setNumeroPortas(Integer numeroPortas) {
+        this.numeroPortas = numeroPortas;
+    }
+
+    public Integer getTipoCombustivel() {
+        return tipoCombustivel;
+    }
+
+    public void setTipoCombustivel(Integer tipoCombustivel) {
+        this.tipoCombustivel = tipoCombustivel;
+    }
+
+    public Long getQuilometragem() {
+        return quilometragem;
+    }
+
+    public void setQuilometragem(Long quilometragem) {
+        this.quilometragem = quilometragem;
+    }
+
+    public BigDecimal getValorLocacao() {
+        return valorLocacao;
+    }
+
+    public void setValorLocacao(BigDecimal valorLocacao) {
+        this.valorLocacao = valorLocacao;
+    }
+
+    public Modelo getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(Modelo modelo) {
+        this.modelo = modelo;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Automovel automovel = (Automovel) o;
+        return Objects.equals(id, automovel.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
 }
